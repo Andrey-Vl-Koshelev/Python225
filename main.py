a9c8b8963c32bd6fe3f3c9e4b0f86c640fc03d4d
Andrey-Vl-Koshelev <111304647+Andrey-Vl-Koshelev@users.noreply.github.com>
2022-11-05 13:37:14 +0400

    OOP

--- main.py
+++ main.py
@@ -261,7 +261,339 @@
 # print('Факториал числа:', Numbers.factor(9))
 
 
+# class Weight:
+#
+#     def __init__(self, k=0):
+#         self.__k = k
+#
+#     @property
+#     def kg_k(self):
+#         return self.__k
+#
+#     @kg_k.setter
+#     def kg_k(self, k):
+#         if isinstance(k, int | float):
+#             self.__k = k
+#         else:
+#             print('Килограммы задаются числами')
+#
+#     def to_pounds(self):
+#         return round(self.__k * 2.205, 2)
+#
+# w1 = Weight(12)
+# print(w1.kg_k, 'кг =>', end='')
+# print(w1.to_pounds(), 'фунтов')
+# w1.kg_k = 41.5
+# print(w1.kg_k, 'кг =>', end='')
+# print(w1.to_pounds(), 'фунтов')
 
+
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = 'RUB'
+#     suffix_usd = 'USD'
+#     suffix_eur = 'EUR'
+#
+#     def __init__(self, num, surname, percent, value=0):
+#         self.num = num
+#         self.surname = surname
+#         self.percent = percent
+#         self.value = value
+#         print(f'Счет #{self.num} принадлежит {self.surname} был открыт. ')
+#         print('*' * 50)
+#
+#     def __del__(self):
+#         print('*' * 50)
+#         print(f'Счет #{self.num} принадлежащий {self.surname} был закрыт. ')
+#
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd )
+#         print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.value, Account.rate_eur )
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
+#
+#     def print_balance(self):
+#         print(f'Текущий баланс {self.value} {Account.suffix}')
+#
+#     def print_info(self):
+#         print(f'Информация о счете.')
+#         print('-' * 20)
+#         print(f'#{self.num}')
+#         print(f'Владелец: {self.surname}')
+#         self.print_balance()
+#         print(f'Проценты: {self.percent:.0%}')
+#         print('-' * 20)
+#
+#     def add_percents(self):
+#         self.value += self.value * self.percent
+#         print('Проценты успешно начислены')
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f'К сожалению у вас нет {val} {Account.suffix}')
+#         else:
+#             self.value -= val
+#             print(f'{val} {Account.suffix} было успешно снято!')
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f'{val} {Account.suffix} было успешно добавлено')
+#         self.print_balance()
+#
+#
+#
+#
+# ass = Account('12345', 'Долгих', 0.03, 1000)
+# ass.print_info()
+# ass.convert_to_usd()
+# ass.convert_to_eur()
+# print()
+# Account.set_usd_rate(2)
+# ass.convert_to_usd()
+# Account.set_eur_rate(3)
+# ass.convert_to_eur()
+# print()
+# ass.edit_owner('Дюма')
+# ass.print_info()
+# print()
+# ass.add_percents()
+# print()
+# ass.withdraw_money(3000)
+# print()
+# ass.withdraw_money(100)
+# print()
+# ass.add_money(5000)
+# print()
+# ass.withdraw_money(3000)
+# print()
+
+
+# class Figure:
+#     def __init__(self, color):
+#         self.__color = color
+#
+#     @property
+#     def color(self):
+#         return self.__color
+#
+#     @color.setter
+#     def color(self, c):
+#         self.__color = c
+#
+# class Rectangle(Figure):
+#     def __init__(self, width, height, color):
+#         super().__init__(color)
+#         self.__width = width
+#         self.__height = height
+#
+#     @property
+#     def width(self):
+#         return self.__width
+#
+#     @width.setter
+#     def width(self, w):
+#         if w > 0:
+#             self.__width = w
+#         else:
+#             raise ValueError
+#
+#     @property
+#     def height(self):
+#         return self.__height
+#
+#     @height.setter
+#     def height(self, h):
+#         if h > 0:
+#             self.__height = h
+#         else:
+#             raise ValueError
+#
+#     def area(self):
+#         print(f'Площадь {self.color} прямоугольника: ', end='')
+#         return self.__width * self.__height
+#
+# rect = Rectangle(10, 20, 'green')
+# # rect.width = 5
+# print(rect.width)
+# print(rect.height)
+# print(rect.color)
+# rect.color = 'red'
+# print(rect.color)
+# print(rect.area())
+
+
+# import math
+# #
+# #
+# # class Table:
+# #     def __init__(self, width=None, length=None, radius=None):
+# #         if radius is None:
+# #             self._width = width
+# #             self._length = length
+# #         else:
+# #             self._radius = radius
+# #
+# #     def calc_area(self):
+# #         raise NotImplementedError('В дочернем классе должен быть определен метод calc_area')
+# #
+# #
+# # class SqTable(Table):
+# #     def calc_area(self):
+# #         return self._width * self._length
+# #
+# #
+# # class RoundTable(Table):
+# #     def calc_area(self):
+# #         return round(math.pi * self._radius ** 2, 2)
+# #
+# #
+# # t = SqTable(20, 10)
+# # print(t.__dict__)
+# # print(t.calc_area())
+# # t2 = RoundTable(radius=20)
+# # print(t2.__dict__)
+# # print(t2.calc_area())
+
+# from abc import ABC, abstractmethod
+#
+#
+# class Currency(ABC):
+#     def __init__(self, value):
+#         self.value = value
+#
+#     @abstractmethod
+#     def convert_to_rub(self):
+#         pass
+#
+#     def print_value(self):
+#         print(self.value, end=' ')
+#
+#
+# class Dollar(Currency):
+#     rate_to_rub = 74.16
+#     suffix = 'USD'
+#
+#     def convert_to_rub(self):
+#         rub = self.value * Dollar.rate_to_rub
+#         return rub
+#
+#     def print_value(self):
+#         super().print_value()
+#         print(Dollar.suffix, end=' ')
+#
+#
+# class Euro(Currency):
+#     rate_to_rub = 90.14
+#     suffix = 'EUR'
+#
+#     def convert_to_rub(self):
+#         rub = self.value * Euro.rate_to_rub
+#         return rub
+#
+#     def print_value(self):
+#         super().print_value()
+#         print(Euro.suffix, end=' ')
+#
+#
+# d = [Dollar(5), Dollar(10), Dollar(50), Dollar(100)]
+# e = [Euro(5), Euro(10), Euro(50), Euro(100)]
+# print('*' * 30)
+# for elem in d:
+#     elem.print_value()
+#     print(f'= {elem.convert_to_rub():.2f} RUB')
+# print('*' * 30)
+# for elem in e:
+#     elem.print_value()
+#     print(f'= {elem.convert_to_rub():.2f} RUB')
+# print('*' * 30)
+
+
+# class Liguid:
+#     def __init__(self, name, density):
+#         self.name = name
+#         self.density = density
+#
+#     def edit_density(self, val):
+#         self.density = val
+#
+#     def calc_v(self, m):
+#         v = m / self.density
+#         print(f'Объем {m} кг {self.name} равен {v} m^3')
+#
+#     def calc_m(self, v):
+#         m = v * self.density
+#         print(f'Вес {v} m^3 of {self.name} составляет {m} кг.')
+#
+#     def print_info(self):
+#         print(f'Жидкость "{self.name}" (плотность = {self.density}kg/m^3).')
+#
+#
+# class Alcohol(Liguid):
+#     def __init__(self, name, density, strength):
+#         super().__init__(name, density)
+#         self.strength = strength
+#
+#     def edit_strength(self, val):
+#         self.strength = val
+#
+#
+# a = Alcohol('Wine', 1064.2, 14)
+# a.print_info()
+# a.edit_density(1000)
+# a.print_info()
+# a.calc_v(300)
+# a.calc_m(0.5)
+# print(a.strength)
+# a.edit_strength(20)
+# print(a.strength)
+
+
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+#         self.note = self.Notebook()
+#
+#     def set_name_notebook(self):
+#         print(f'{self.name} => ', end=' ')
+#         self.note.set_notebook_all()
+#
+#     class Notebook:
+#         def __init__(self):
+#             self.model = 'HP'
+#             self.processor = 'i7'
+#             self.memory = 16
+#
+#         def set_notebook_all(self):
+#             print(f'{self.model}, {self.processor}, {self.memory}')
+#
+#
+# s1 = Student('Roman')
+# s2 = Student('Vova')
+# s1.set_name_notebook()
+# s2.set_name_notebook()
 
 
 
