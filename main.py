5cc70d16409b87bfdb6427b3dd49f5993d3d26dd
Andrey-Vl-Koshelev <111304647+Andrey-Vl-Koshelev@users.noreply.github.com>
2022-11-20 13:30:40 +0400

    OOP

--- main.py
+++ main.py
@@ -1530,11 +1530,126 @@ import csv
 # #     for d in data:
 # #         writer.writerow(d)
 
+# import requests
+# import csv
+#
+# with open('DZ/data2.csv', 'r') as f:
+#     print(f.read())
+
+# from bs4 import BeautifulSoup
+
+# f = open('index.html').read()
+# soup = BeautifulSoup(f, 'html.parser')
+# row = soup.find('div', class_='name').text
+# row = soup.find_all('div', class_='name')
+# row = soup.find_all('div', {'class':'name'})
+# row = soup.find('div', {'data-set':'salary'})
+# row = soup.find('div', text='Alena').parent
+# row = soup.find('div', text='Alena').find_parent(class_='row')
+# row = soup.find('div', id='whois3').find_next_sibling()
+# row = soup.find('div', id='whois3').find_previous_sibling()
+# print(row)
+# def get_copywriter(tag):
+#     whois = tag.find('div', class_='whois').text
+#     if 'Copywriter' in whois:
+#         return tag
+#     return None
+#
+#
+# f = open('index.html', encoding='utf-8').read()
+# soup = BeautifulSoup(f, 'html.parser')
+# copywriter = []
+# row = soup.find_all('div', class_='row')
+# for i in row:
+#     cw = get_copywriter(i)
+#     if cw:
+#         copywriter.append(cw)
+# print(copywriter)
+# import re
+#
+#
+# def get_salary(s):
+#     pattern = r'\d+'
+#     # res = re.findall(pattern, s)[0]
+#     res = re.search(pattern, s).group()
+#     print(res)
+#
+#
+# f = open('index.html', encoding='utf-8').read()
+# soup = BeautifulSoup(f, 'html.parser')
+# salary = soup.find_all('div', {'data-set': 'salary'})
+# for i in salary:
+#     get_salary(i.text)
+
 import requests
+from bs4 import BeautifulSoup
+import re
 import csv
 
-with open('DZ/data2.csv', 'r') as f:
-    print(f.read())
+
+# reg = requests.get('https://ru.wordpress.org/')
+# # print(reg.headers['Content-Type'])
+# # reg.encoding = 'utf-8'
+# print(reg.text)
 
 
+# def get_data(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     p1 = soup.find('header', id='masthead').find('p', class_='site-title')
+#     return p1
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/'
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
 
+# def refined(s):
+#     res = re.sub(r'\D+', '', s)
+#     return res
+#
+#
+# def write_csv(data):
+#     with open('plugins.csv', 'a') as f:
+#         writer = csv.writer(f, lineterminator='\r', delimiter=';')
+#         writer.writerow((data['name'], data['url'], data['rating']))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     p1 = soup.find_all('section', class_='plugin-section')[1]
+#     plugins = p1.find_all('article')
+#     for plugin in plugins:
+#         name = plugin.find('h3').text
+#         # url = plugin.find('h3').find('a')['href']
+#         url = plugin.find('h3').find('a').get('href')
+#         rating = plugin.find('span', class_='rating-count').find('a').text
+#         r = refined(rating)
+#         data = {'name': name, 'url': url, 'rating': r}
+#         write_csv(data)
+#         # print(data)
+#
+#         # print(rating)
+#     # return len(plugins)
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/plugins/'
+#     get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
