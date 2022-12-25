06e2258254b72885a1503cb679933709cee57b6d
Andrey-Vl-Koshelev <111304647+Andrey-Vl-Koshelev@users.noreply.github.com>
2022-12-25 19:23:29 +0400

    OOP

--- main.py
+++ main.py
@@ -3829,15 +3829,24 @@
 # cur.executemany('INSERT INTO garden VALUES(NULL, ?, ?, ?)', text)
 # for i in text:
 #     cur.execute('INSERT INTO garden VALUES(NULL,?,?,?)', i)
+# import os
+#
+# from models.database import DATABASE_NAME
+# import create_database as db_creator
+#
+# if __name__ == '__main__':
+#     db_is_created = os.path.exists(DATABASE_NAME)
+#     if not db_is_created:
+#         db_creator.create_database()
+
 import os
 
-from models.database import DATABASE_NAME
-import create_database as db_creator
+from mytables.my_database import DATABASE_NAME
+import mytable_database as db_mytable
 
 if __name__ == '__main__':
     db_is_created = os.path.exists(DATABASE_NAME)
     if not db_is_created:
-        db_creator.create_database()
-
+        db_mytable.mytable_database()
 
 
---
+++ mytable_database.py
@@ -0,0 +1,40 @@
+from faker import Faker
+
+from mytables.my_database import create_db, Session
+from mytables.name import Name
+from mytables.number import Number
+
+
+def mytable_database(load_fake_data=True):
+    create_db()
+    if load_fake_data:
+        _load_fake_data(Session())
+
+
+def _load_fake_data(session):
+    lessons_names = ['Тактика', 'История', 'Стрельба', 'Эксплуатация вооружения',
+                     'Высшая математика', 'Сопротивление материалов']
+
+    group1 = Number(group_name='WELL-221')
+    group2 = Number(group_name='WELL-222')
+    session.add(group1)
+    session.add(group2)
+    lesson = Number.lesson_title
+
+    for it in lessons_names:
+        lesson += it
+        session.add(lesson)
+
+    faker = Faker('ru_RU')
+    group_list = [group1, group2]
+    session.commit()
+
+    for _ in range(40):
+        full_name = faker.name().split()
+        age = faker.random.randint(17, 27)
+        group = faker.random.choice(group_list)
+        cadet = Name(full_name, age, group.id)
+        session.add(cadet)
+
+    session.commit()
+    session.close()
